f5ff18248143fb408d9938ac97f76bb71ba354db
akshaya-biswal <akshaya.join@gmail.com>
2023-04-01 15:43:21 +0530

    Linear Seearch

--- Array/Searching/Linear/code.js
+++ Array/Searching/Linear/code.js
@@ -1,12 +1,31 @@
+const array = [10, 20, 30, 40, 50];
+
 function linearSearch(arr, target) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
       return i;
     }
   }
   return -1;
 }
 
-const array = [10, 20, 30, 40, 50];
-
 console.log(linearSearch(array, 40)); // 3
+
+// --------------------------------------------------
+
+function linearSearchRecursive(arr, target) {
+  const length = arr.length - 1;
+
+  if (length === 0) {
+    return -1;
+  }
+
+  if (arr[length] === target) {
+    return length;
+  } else {
+    arr.pop();
+    return linearSearchRecursive(arr, target);
+  }
+}
+
+console.log(linearSearchRecursive(array, 40)); // 3
