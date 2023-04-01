f25c5dadfba14bffba4de899e67328c1f1b6ed24
akshaya-biswal <akshaya.join@gmail.com>
2023-04-01 17:40:44 +0530

    Search

--- Array/Searching/Binary/code.js
+++ Array/Searching/Binary/code.js
@@ -1,3 +1,23 @@
 const arr = [10, 20, 30, 40, 50];
 
-function binarySearch(array, target) {}
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+    if (array[middle] === target) {
+      return middle;
+    }
+
+    if (array[middle] > target) {
+      right = middle + 1;
+    } else {
+      left = middle - 1;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearch(arr, 40));
