c52ce106c9c883832337d134b7754bf0464c2415
akshaya-biswal <akshaya.join@gmail.com>
2023-04-21 16:39:15 +0530

    Binary Tree Added

--- Tree/BinaryTree/code.js
+++ Tree/BinaryTree/code.js
@@ -153,13 +153,3 @@ console.log("Search", bst.search(bst.root, 3));
 
 bst.delete(10);
 bst.levelOrder(bst.root); // 15 - 5 - 3 - 7
-
-const tree = new BST();
-tree.root = new Node(1);
-tree.root.left = new Node(2);
-tree.root.right = new Node(3);
-tree.root.left.left = new Node(4);
-tree.root.left.right = new Node(5);
-tree.root.right.left = new Node(6);
-tree.root.right.right = new Node(7);
-console.log("Full Binary Tree", bst.isFullTree(bst.root));
---
+++ Tree/BinaryTree/full_binary_tree.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  isEmpty() {
+    return this.root === null;
+  }
+
+  isFullTree(node) {
+    // if empty tree
+    if (node === null) {
+      return true;
+    }
+
+    // if leaf node
+    if (node.left === null && node.right === null) {
+      return true;
+    }
+
+    // if both left and right subtrees are not null
+    if (node.left !== null && node.right !== null) {
+      return this.isFullTree(node.left) && this.isFullTree(node.right);
+    }
+
+    // if none work
+    return false;
+  }
+}
+
+const tree = new BST();
+tree.root = new Node(1);
+tree.root.left = new Node(2);
+tree.root.right = new Node(3);
+tree.root.left.left = new Node(4);
+tree.root.left.right = new Node(5);
+tree.root.right.left = new Node(6);
+tree.root.right.right = new Node(7);
+console.log("Full Binary Tree", tree.isFullTree(tree.root));
